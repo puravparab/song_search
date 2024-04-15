2ec71dd941157ec2332ea3559ace50a67a1082db
Purav <puravparabdev@gmail.com>
2024-04-15 15:18:51 -0700

    added client/

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between py-20 px-14">
+      <h1 className="text-3xl text-bold">Song Search</h1>
+      <p></p>
+    </main>
+  );
+}
+
+export default Home;
